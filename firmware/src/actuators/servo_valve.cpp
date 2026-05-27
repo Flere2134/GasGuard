8bc8414140478c3e96d0d6f5a0714d7d7065826d
Flere21 <aaronhans2134@gmail.com>
2026-05-27 16:07:00 +0800

    servo calibrated

--- firmware/src/actuators/servo_valve.cpp
+++ firmware/src/actuators/servo_valve.cpp
@@ -20,8 +20,8 @@ static const int SERVO_MAX_US       = 2400;
 // IMPORTANT: You must manually calibrate these numbers!
 // These define exactly how long the motor spins. 
 // For example, if 450ms turns the valve exactly 90 degrees, set it to 450.
-static const int TIME_TO_OPEN_MS  = 250; 
-static const int TIME_TO_CLOSE_MS = 170;
+static const int TIME_TO_OPEN_MS  = 750;
+static const int TIME_TO_CLOSE_MS = 500;
 
 // ==========================================
 // haltAndDetachServo()
@@ -30,8 +30,8 @@ static const int TIME_TO_CLOSE_MS = 170;
 // avoid continuous creeping or stalling.
 // ==========================================
 static void haltAndDetachServo() {
-  valveServo.write(90); // 90 is the theoretical stop point for 360 servos
-  delay(300);            // Brief pause to let the motor brake
+  valveServo.write(94); // 90 is the theoretical stop point for 360 servos
+  delay(400);            // Brief pause to let the motor brake
   valveServo.detach();  // Cut the signal entirely
   Serial.println("[SERVO] Signal detached. Motor locked in position.");
 }
@@ -58,12 +58,13 @@ void openValve() {
   if (currentValveState == VALVE_OPEN) return;
 
   Serial.println("[SERVO] Opening valve...");
-  
-  // 1. Re-attach the signal
   valveServo.attach(PIN_SERVO, SERVO_MIN_US, SERVO_MAX_US);
+  // 1. Pre-load the speed and direction FIRST
+  valveServo.write(108); 
   
-  // 2. Spin forward (adjust 180 to a lower number like 110 for slower speed)
-  valveServo.write(10); 
+  // 2. THEN re-attach the signal wire. 
+  // This prevents the default 90 pulse from firing.
+  
   
   // 3. Wait for the precise calibrated time
   delay(TIME_TO_OPEN_MS);
@@ -82,12 +83,12 @@ void closeValve() {
   if (currentValveState == VALVE_CLOSED) return;
 
   Serial.println("[SERVO] *** CLOSING VALVE — GAS LEAK DETECTED ***");
-  
-  // 1. Re-attach the signal
   valveServo.attach(PIN_SERVO, SERVO_MIN_US, SERVO_MAX_US);
+  // 1. Pre-load the speed and direction FIRST
+  valveServo.write(80); 
   
-  // 2. Spin backward (adjust 0 to a higher number like 70 for slower speed)
-  valveServo.write(0); 
+  // 2. THEN re-attach the signal wire.
+  
   
   // 3. Wait for the precise calibrated time
   delay(TIME_TO_CLOSE_MS);
